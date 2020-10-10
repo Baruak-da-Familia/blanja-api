74ef5137b7afba956b3d544173534d1d57e82b26
AhmadZaky19 <a.zaky32@gmail.com>
2020-10-10 13:21:53 +0700

    install packages

---
+++ src/Controllers/auth.js
@@ -0,0 +1,27 @@
+const formResponse = require("../Helpers/Forms/formResponse");
+const authModel = require("../Models/auth");
+
+const authController = {
+  register: (req, res) => {
+    authModel
+      .register(req.body)
+      .then((data) => {
+        formResponse.success(res, data, 200);
+      })
+      .catch((err) => {
+        formResponse.error(res, err, 500);
+      });
+  },
+  login: (req, res) => {
+    authModel
+      .login(req.body)
+      .then((data) => {
+        formResponse.success(res, data, 200);
+      })
+      .catch((err) => {
+        formResponse.error(res, err, 500);
+      });
+  },
+};
+
+module.exports = authController;
