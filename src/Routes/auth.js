a8cd2ac9b7f9857fab7cb434340c77d304090be8
AhmadZaky19 <a.zaky32@gmail.com>
2020-10-12 11:31:20 +0700

    add auth

--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -2,7 +2,7 @@ const authRouter = require("express").Router();
 const authController = require("../Controllers/auth");
 
 authRouter.post("/register/customer", authController.customerRegister);
-authRouter.post("/sellerregister", authController.sellerRegister);
+authRouter.post("/register/seller", authController.sellerRegister);
 authRouter.post("/login/customer", authController.customerLogin);
 authRouter.post("/login/seller", authController.sellerLogin);
 
