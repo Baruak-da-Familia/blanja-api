19da82497626ce798bfd9dee6179f0c9c02ed12e
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-10-14 09:37:14 +0700

    [update] add transaction and get transaction history

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authRouter = require("./auth");
 const userRouter = require("./user");
+const transactionRouter = require("./transaction");
 
 router.use("/auth", authRouter);
 router.use("/", userRouter);
+router.use("/order", transactionRouter);
 
 module.exports = router;
---
+++ src/Routes/transaction.js
@@ -0,0 +1,19 @@
+const { express } = require("../../sharedVariable");
+
+const transactionController = require("../Controllers/transaction");
+
+const transactionRouter = express.Router();
+
+//add transaction
+transactionRouter.post("/", transactionController.addTransaction);
+transactionRouter.get(
+	"/customer/:id",
+	transactionController.getTransactionCustomer
+);
+transactionRouter.get(
+	"/seller/:id",
+	transactionController.getTransactionSeller
+);
+// transactionRouter.post("/order", transactionController.addOrder);
+
+module.exports = transactionRouter;
