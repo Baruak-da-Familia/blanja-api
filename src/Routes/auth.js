93803fb6939621f95613b5dedd6054abf5bb7c2f
Faiz Sulistyawan <faiz.sulistyawan@gmail.com>
2020-10-26 13:45:48 +0700

    latest update

--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -7,6 +7,9 @@ authRouter.post("/register/seller", authController.sellerRegister);
 authRouter.post("/login/customer", authController.customerLogin);
 authRouter.post("/login/seller", authController.sellerLogin);
 authRouter.post('/sendemailcustomer', authController.sendEmailCustomer)
+authRouter.post('/sendemailseller', authController.sendEmailSeller)
 authRouter.post('/resetpasscustomer', authController.customerReset)
+authRouter.post('/resetpassseller', authController.sellerReset)
+
 
 module.exports = authRouter;
